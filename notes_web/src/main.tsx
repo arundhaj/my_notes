e137dbbe2a88c11077491e66a374884b80f0535a
Arunkumar Dharuman <arundhaj@gmail.com>
2026-08-27 22:51:57 +0530

    Removed Roboto font

--- notes_web/src/main.tsx
+++ notes_web/src/main.tsx
@@ -1,10 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// MUI's default typography asks for Roboto; these self-host the weights it uses.
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
 import App from './App.tsx'
 
